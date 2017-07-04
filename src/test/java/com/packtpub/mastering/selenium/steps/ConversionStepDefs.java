7847037b2a711fa6d78bbf4cad83c35ab511984d
DESKTOP-F2HHP1C\OZGUR-FAIRBIT <ozgur.altuntas@fairbit.com>
2017-07-03 18:11:24 -0700

    reverse back to see same error

--- src/test/java/com/packtpub/mastering/selenium/steps/ConversionStepDefs.java
+++ src/test/java/com/packtpub/mastering/selenium/steps/ConversionStepDefs.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+//import static org.junit.Assert.fail;
 
 
 /**
@@ -21,7 +21,7 @@ public class ConversionStepDefs {
     @Given("^I want to  convert (\\d+).(\\d+) degree Fahrenheit to Celsius$")
     public void I_want_to_convert_degree_Fahrenheit_to_Celsius(int arg1, int arg2) throws Throwable {
         // Express the Regexp above with the code you wish you had
-        fail("It is failed");
+        //fail("It is failed");
         throw new PendingException();
     }
 
